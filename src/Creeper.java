0a8192d63e2b75f0d661ba98431dd3f7dfb3c971
Kendra Kleber <league@Mac-mini-2.local>
2017-07-15 11:35:31 -0700

    creeper

---
+++ src/Creeper.java
@@ -0,0 +1,43 @@
+import java.awt.JobAttributes;
+
+import javax.swing.JOptionPane;
+
+public class Creeper {
+public static void main(String[] args) {
+JOptionPane.showInputDialog( " Whats your name ?");
+String name = JOptionPane.showInputDialog(" hi my name is Melissa i am calling to say that you have a doctors's  apointmet this afternoon ");
+String address=JOptionPane.showInputDialog(" Can i have your address so i can deliver you a suprise");
+String Phonenumber= JOptionPane.showInputDialog(" What is your phone number you will get a unexpected person calling you");
+String Birthday = JOptionPane.showInputDialog( "When is your birthday i dont want to miss your special day");
+JOptionPane.showMessageDialog(null,  "your name is" + name+ address+ Phonenumber+Birthday );
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
---
+++ src/Turtle.java
@@ -0,0 +1,25 @@
+import javax.swing.JOptionPane;
+
+public class Turtle {
+public static void main(String[] args) {
+JOptionPane.showMessageDialog(null, "hi");
+JOptionPane.showInputDialog( "");
+String turtle =JOptionPane.showInputDialog( "what is your name");
+ JOptionPane.showMessageDialog(null, "I like turtles do you?"+ turtle);
+String favorite= JOptionPane.showInputDialog( "What is your favorite dessert");
+JOptionPane.showMessageDialog(null, favorite + "that is the best");
+	 
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
+}
